0084f3e0eef38d06b1ddc4e9ba97040f1abc2db8
mostafa <mostafa.balata@gmail.com>
2016-05-10 15:49:25 +0300

    frappe developer

--- frappe/patches/v6_24/set_language_as_code.py
+++ frappe/patches/v6_24/set_language_as_code.py
@@ -2,6 +2,7 @@ import frappe
 
 from frappe.translate import get_lang_dict
 
+
 # migrate language from name to code
 def execute():
 	language = frappe.db.get_value('System Settings', None, 'language')
